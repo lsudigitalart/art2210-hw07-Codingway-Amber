acd8da359f087dd72595c81a6aa151fe50ce3d43
Codingway-Amber <amberreine7@gmail.com>
2024-10-21 13:26:57 -0500

    added music and got it to play in chrome

--- sketch.js
+++ sketch.js
@@ -1,7 +1,45 @@
+var everglow;
+var soundPlayed = false;
+// var playTime, loadTime;
+var startTime
+var amp, level;
+var fft;
+
+
+function preload() {
+  everglow = loadSound("everglow.mp3");
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 700);
+  getAudioContext().suspend();
+  startTime = millis();
+  
+  // getAudioContext().resume();
+
+  // if (everglow.isLoaded()) {
+  //   loadTime = millis();
+  //   print(loadTime);
+  //   everglow.play();
+  // }
+
+  amp = new p5.Amplitude();
+  fft = new p5.FFT();
+}
+
+function mousePressed() {
+  userStartAudio();
 }
 
 function draw() {
   background(220);
+
+  if (!soundPlayed) {
+    everglow.play();
+    soundPlayed = true;
+}
+
+if (millis() - startTime > 3000) {
+  ellipse(width / 2, height / 2, 50);
+}
 }
