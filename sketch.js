aba0c4ffeaab15adbab9a229846e3df5e4303d8a
Codingway-Amber <amberreine7@gmail.com>
2024-10-21 14:15:34 -0500

    adjusted some code

--- sketch.js
+++ sketch.js
@@ -1,20 +1,22 @@
 var everglow;
 var soundPlayed = false;
-var startTime
+var startTime;
 var amp, level;
-var fft;
+var soundFile, fft;
 
 function preload() {
   everglow = loadSound("everglow.mp3");
+  soundFile = loadSound('everglow.mp3');
 }
 
 function setup() {
   createCanvas(700, 580);
   getAudioContext().suspend();
   startTime = millis();
-
   amp = new p5.Amplitude();
   fft = new p5.FFT();
+  fft.setInput(soundFile);
+  soundFile.play(); 
 }
 
 function mousePressed() {
@@ -23,13 +25,21 @@ function mousePressed() {
 
 function draw() {
   background(220);
+  var spectrum = fft.analyze();
+  var bassEnergy = fft.getEnergy("bass");
+  var threshold = 200;
 
   if (!soundPlayed) {
     everglow.play();
     soundPlayed = true;
-}
+  }
+  
+  if (bassEnergy > threshold) {
+    fill(255, 0, 0);
+    ellipse(width / 2, height / 2, 50, 50);
+  }
 
-// if (millis() - startTime > 3000) {
-//   ellipse(width / 2, height / 2, 50);
-//   }
+  // if (millis() - startTime > 3000) {
+  //   ellipse(width / 2, height / 2, 50);
+  //   }
 }
