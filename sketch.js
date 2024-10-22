ebf1dfd4c405602d396de2ade9f9c4636fa49c61
Codingway-Amber <amberreine7@gmail.com>
2024-10-21 21:24:33 -0500

    completed omg

--- sketch.js
+++ sketch.js
@@ -6,6 +6,17 @@ var amp, level;
 var soundFile, fft;
 var stars;
 var space;
+var lyrics = [
+  { text: "So close to the glow", startTime: 1000, endTime: 5000 },
+  { text: "Headed to light that I will hold", startTime: 5500, endTime: 10000 },
+  { text: "Now it's time to gather what's lost", startTime: 11700, endTime: 16500 },
+  { text: "Show me how stars fall down", startTime: 16500, endTime: 19500 },
+  { text: "And still shine bright", startTime: 19500, endTime: 22500 },
+  { text: "Wherever you go", startTime: 23500, endTime: 25500 },
+  { text: "There I'll be your sight", startTime: 25500, endTime: 28500 },
+  { text: "So that we can seek on through the night", startTime: 28500, endTime: 36500 },
+  { text: "These are words I'll keep through our time", startTime: 36500, endTime: 46500 }
+];
 
 function preload() {
   everglow = loadSound("everglow.mp3");
@@ -76,9 +87,25 @@ function draw() {
   circle(322, 484, trebleVol);
   circle(1043, 484, trebleVol);
 
-  push()
-  fill(255)
-  text(mouseX, 0, 240); 
-  text(mouseY, 0, 260);
-  pop()
+  displayLyrics();
+
+  // push()
+  // fill(255)
+  // text(mouseX, 0, 240); 
+  // text(mouseY, 0, 260);
+  // pop()
+}
+
+function displayLyrics() {
+  let currentTime = millis() - startTime;
+
+  for (let i = 0; i < lyrics.length; i++) {
+    let lyric = lyrics[i];
+    if (currentTime > lyric.startTime && currentTime < lyric.endTime) {
+      fill(255); // Set text color to white
+      textSize(32);
+      textAlign(CENTER, CENTER);
+      text(lyric.text, width / 2, height / 2);
+    }
+  }
 }
